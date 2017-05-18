d6db71ec2b093edb603312fa075be94a7447affa
victor <vico.16c@gmail.com>
2017-05-18 11:16:02 -0500

    cambio

---
+++ src/Components/List.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <ul>
+          <li>uno</li>
+          <li>dos</li>
+      </ul>
+    );
+  }
+}
+
+export default List;
