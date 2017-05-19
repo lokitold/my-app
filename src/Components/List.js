8e2925b7effcfe29cdec6846d9bda03c4a67f854
victor <vico.16c@gmail.com>
2017-05-19 12:37:12 -0500

    cambi

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,22 +1,44 @@
 import React, {Component} from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 
 class List extends Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
             date: new Date(),
-            names : ['Jake', 'Jon', 'Thruster']
+            names: ['Jake', 'Jon', 'Thruster']
         };
+    }
+
+    componentDidMount() {
+        axios.get('https://m7s0kugbb6.execute-api.us-east-1.amazonaws.com/dev/todos')
+            .then(response => {
+                this.setState({
+                    posts: response
+                });
+                //console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
 
     render() {
 
-        var namesList = this.state.names.map(function(name){
-            return <li>{name}</li>;
-        })
+        var namesList = this.state.names.map(function (name) {
+            return <li key={name}>{name}</li>;
+        });
+
+        var postList = [];
+        if (this.state.posts !== undefined) {
+            //console.log(this.state.posts);
+            postList = Object.keys(this.state.posts.data).map((key)  => {
+                return <li key={key}>{this.state.posts.data[key].body}</li>;
+            })
+        }
 
         return (
             <ul>
@@ -26,6 +48,7 @@ class List extends Component {
                 <li>{this.props.name}</li>
                 <li>{this.state.date.toLocaleTimeString()}</li>
                 {namesList}
+                {postList}
             </ul>
         );
     }
