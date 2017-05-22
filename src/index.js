1a51714fac6f4c6efef4b00a4a076460eee77290
victor <vico.16c@gmail.com>
2017-05-22 16:24:07 -0500

    cambio

--- src/index.js
+++ src/index.js
@@ -2,8 +2,71 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './Components/List';
 import './index.css';
+import { BrowserRouter,Switch,Route,Link } from 'react-router-dom'
 
-ReactDOM.render(
-  <List name="sara" />,
-  document.getElementById('root')
+
+const Home = () => (
+    <div>
+        <h1>Welcome to the Tornadoes Website!</h1>
+    </div>
+)
+
+const Roster = () => (
+    <Switch>
+        <h1>Welcome to Roster</h1>
+    </Switch>
+)
+
+const Schedule = () => (
+    <div>
+        <ul>
+            <li>6/5 @ Evergreens</li>
+            <li>6/8 vs Kickers</li>
+            <li>6/14 @ United</li>
+        </ul>
+    </div>
+)
+
+const Header = () => (
+    <header>
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/roster'>Roster</Link></li>
+                <li><Link to='/schedule'>Schedule</Link></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+const Main = () => (
+    <main>
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/roster' component={Roster}/>
+            <Route path='/schedule' component={Schedule}/>
+        </Switch>
+    </main>
+)
+
+
+const App = () => (
+    <div>
+        <Header />
+        <Main />
+    </div>
 );
+
+
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root2'));
+
+
+ReactDOM.render((
+    <BrowserRouter>
+        <List name="sara" />
+    </BrowserRouter>
+    ), document.getElementById('root'));
