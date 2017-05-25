273bb451144170905d11f3066c95002cc9789cf4
victor <vico.16c@gmail.com>
2017-05-25 13:07:58 -0500

    sdfsdf

--- index.js
+++ index.js
@@ -13,16 +13,27 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/dynamic', function (req, res) {
 
-	var url = "http://wowcrush.com/images/"
-	var random = function (min, max) {
-	  return Math.random() * (max - min) + min;
-	}
+	// var url = "http://wowcrush.com/images/"
+	// var random = function (min, max) {
+	//   return Math.random() * (max - min) + min;
+	// }
+    //
+	// var number = parseInt(random(1,31241));
+    //
+	// var image = url + number + '.jpg';
 
-	var number = parseInt(random(1,31241));
 
-	image = url + number + '.jpg';
+    var image = [
+        'http://wowcrush.com/images/358425.jpg',
+        'http://wowcrush.com/images/656436.jpg',
+        'http://wowcrush.com/images/684020.jpg',
+        'http://wowcrush.com/images/182168.jpg',
+        'http://wowcrush.com/images/267959.jpg'
+    ];
 
-    res.render('index', { image: image, message: 'Hello there!'});
+    var rand = image[Math.floor(Math.random() * image.length)];
+
+    res.render('index', { image: rand, message: 'Hello there!'});
 });
 
 
